774bf969a80d4f9ed6854f02cdab6a690afad55d
thalials <thalials@al.insper.edu.br>
2022-05-10 13:45:11 -0300

    inicio das paredes com porta

--- game-explorer/Assets/Scripts/PlayerController.cs
+++ game-explorer/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     float _baseSpeed = 10.0f;
+    float _gravidade = 9.8f;
     CharacterController characterController;
+
+    //Referência usada para a câmera filha do jogador
+    GameObject playerCamera;
+    //Utilizada para poder travar a rotação no angulo que quisermos.
+    float cameraRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        playerCamera = GameObject.Find("Main Camera");
+        cameraRotation = 0.0f;
     }
 
     // Update is called once per frame
@@ -19,8 +27,24 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(x, 0, z);
+        //Verificando se é preciso aplicar a gravidade
+       float y = 0;
+       if (!characterController.isGrounded){
+           y = -_gravidade;
+       }
+
+        //Tratando movimentação do mouse
+       float mouse_dX = Input.GetAxis("Mouse X");
+       float mouse_dY = Input.GetAxis("Mouse Y");
+
+        //Tratando a rotação da câmera
+        cameraRotation += mouse_dY;
+        cameraRotation = Mathf.Clamp(cameraRotation, -75.0f, 75.0f);
+
+        Vector3 direction = transform.right * x + transform.up * y + transform.forward * z;
 
         characterController.Move(direction * _baseSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, mouse_dX);
+        playerCamera.transform.localRotation = Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
     }
 }
